1a84bd829d9b69045a0e92a2ec73597195664b75
David Waltenburg <david@waltenburg.dk>
2024-04-26 17:37:36 +0200

    Styled city cards

--- city-info/src/components/Pages/Cities/CitiesCards.tsx
+++ city-info/src/components/Pages/Cities/CitiesCards.tsx
@@ -1,30 +1,65 @@
 import React from "react";
-import { Button, ButtonGroup, Card } from "react-bootstrap";
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 import { Icon } from "../../Icon";
 import { CityListProps, City } from "../../Props/CityProps";
-import { POIColor } from "../../Colors";
+import { POIColor, LanguageColor } from "../../Colors";
 import { Link } from "react-router-dom";
 
 // TODO: theme cards to the city theme color
 let CityCard = ({ ...props }: City) => {
   return (
-    <Card as={Link} to={"/Cities/" + props.cityId} className="my-2 normal-text">
+    <Card
+      as={Link}
+      to={"/Cities/" + props.cityId}
+      className="my-2 normal-text city-card"
+    >
       <Card.Header>
         <Card.Title>{props.name}</Card.Title>
         <Card.Subtitle>{props.country.countryName}</Card.Subtitle>
       </Card.Header>
       <Card.Body>
-        <Card.Text>{props.description}</Card.Text>
         <Card.Text>
-          {props.numberOfPointsOfInterest} <Icon iconName="PinMapFill" color={POIColor} />
+        {props.description}
         </Card.Text>
+        <div style={{display: "flex", gap:"1rem", justifyContent: "flex-end"}}>
+          <OverlayTrigger
+            overlay={
+              <Tooltip id="tooltip-disabled">Languages</Tooltip>
+            }
+            placement="right"
+          >
+            <div style={{width: "fit-content"}}>
+              {props.cityLanguages.length}{" "}
+              <Icon iconName="ChatTextFill" color={LanguageColor} />
+            </div>
+          </OverlayTrigger>
+          <OverlayTrigger
+            overlay={
+              <Tooltip id="tooltip-disabled">
+                Points of Interest
+              </Tooltip>
+            }
+            placement="right"
+          >
+            <div style={{width: "fit-content"}}>
+              {props.numberOfPointsOfInterest}{" "}
+              <Icon iconName="PinMapFill" color={POIColor} />
+            </div>
+          </OverlayTrigger>
+          </div>
       </Card.Body>
       <Card.Footer>
         <ButtonGroup>
-          <Button variant="outline-primary" href="#">
+          <Button variant="outline-primary" href="#"> {/* go to edit city page */}
             <Icon iconName="Pencil" /> Edit
           </Button>
-          <Button variant="outline-danger" href="#">
+          <Button variant="outline-danger" href="#"> {/* open modal "u sure, brah?" */}
             <Icon iconName="XLg" /> Delete
           </Button>
         </ButtonGroup>
