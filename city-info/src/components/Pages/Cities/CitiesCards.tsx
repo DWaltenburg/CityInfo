6da7160210b1098557eea41074991e38b280e338
David Waltenburg <david@waltenburg.dk>
2024-06-17 09:16:43 +0200

    Added deletion of cities and modal for confirmation

--- city-info/src/components/Pages/Cities/CitiesCards.tsx
+++ city-info/src/components/Pages/Cities/CitiesCards.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   ButtonGroup,
   Card,
+  Modal,
   OverlayTrigger,
   Tooltip,
 } from "react-bootstrap";
@@ -10,13 +11,14 @@ import { Icon } from "../../Icon";
 import { CityListProps, City } from "../../Props/CityProps";
 import { POIColor, LanguageColor } from "../../Colors";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import $ from "jquery";
 
-// TODO: theme cards to the city theme color
-let CityCard = ({ ...props }: City) => {
+let CityCard = ({ ...props }: any) => {
   return (
     <Card
-      as={Link}
-      to={"/Cities/" + props.cityId}
+      // as={Link}
+      // to={"/Cities/" + props.cityId}
       className="my-2 normal-text city-card"
     >
       <Card.Header>
@@ -59,7 +61,7 @@ let CityCard = ({ ...props }: City) => {
           <Button variant="outline-primary" href="#"> {/* go to edit city page */}
             <Icon iconName="Pencil" /> Edit
           </Button>
-          <Button variant="outline-danger" href="#"> {/* open modal "u sure, brah?" */}
+          <Button variant="outline-danger" onClick={() => {props.openAndSetDeleteCityModal(props.cityId, props.cityName)}}> {/* open modal "u sure, brah?" */}
             <Icon iconName="XLg" /> Delete
           </Button>
         </ButtonGroup>
@@ -68,14 +70,62 @@ let CityCard = ({ ...props }: City) => {
   );
 };
 
+
+let DeleteCityModal = ({showModal, handleClose, cityId, cityName}: any) => { 
+  let deleteCityById = ()=>{$.ajax({
+    url: "https://cityinfo80.buchwaldshave34.dk/api/City/DeleteCity/" + cityId.toString(),
+    type: "DELETE",
+    success: function (data) {
+      console.log("City deleted");
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });}
+
+  return (
+    <Modal
+    show={showModal}
+    size="sm"
+    centered
+    onHide={handleClose}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Delete City "{cityName}"?</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h1>Are you sure you want to delete this city?</h1>
+      </Modal.Body>
+      <Modal.Footer>
+      <Button variant="danger" onClick={function(event){deleteCityById(); handleClose(); window.location.reload(); /*create alert for success*/}}> 
+        <Icon iconName="Trash" /> Delete
+      </Button>
+      <Button variant="secondary" onClick={handleClose}> 
+        <Icon iconName="X" /> Cancel
+      </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
 let CitiesCards = ({ cities }: CityListProps) => {
+  
+  const [deleteModalShow, setDeleteModalShow] = useState(false);
+  const [deleteCityId, setDeleteCityId] = useState(0);
+  const [deleteCityName, setDeleteCityName] = useState("");
+  const openAndSetDeleteCityModal = (cityId: number, cityName: string) => {
+    setDeleteCityId(cityId);
+    setDeleteCityName(cityName);
+    setDeleteModalShow(true);
+  }
   if (!cities) {
     return <div>No cities to display</div>;
   }
   return (
+    <>
     <div className="container">
       <div className="row">
-        {cities.map((city: City) => {
+        {cities.map((city: any) => {
           return (
             <div className="col-md-4" key={city.cityId}>
               <CityCard
@@ -87,12 +137,15 @@ let CitiesCards = ({ cities }: CityListProps) => {
                 cityName={city.cityName}
                 numberOfPointsOfInterest={city.numberOfPointsOfInterest}
                 pointsOfInterest={city.pointsOfInterest}
+                openAndSetDeleteCityModal={openAndSetDeleteCityModal}
               />
             </div>
           );
         })}
       </div>
     </div>
+    <DeleteCityModal showModal={deleteModalShow} handleClose={()=>setDeleteModalShow(false)} cityId={deleteCityId} cityName={deleteCityName}/>
+    </>
   );
 };
 
